2a3009ca5d30e7b545a38d63332b8681549caea7
john-smilga <johnsmilga@gmail.com>
2020-11-05 11:48:36 -0800

    local storage added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  let theme = 'light-theme'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 function App() {
-  const [theme, setTheme] = useState('light-theme')
+  const [theme, setTheme] = useState(getStorageTheme())
 
   const toggleTheme = () => {
     if (theme === 'light-theme') {
@@ -15,6 +23,7 @@ function App() {
 
   useEffect(() => {
     document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
   }, [theme])
   return (
     <main>
